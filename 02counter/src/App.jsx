b6a4fa5690b4210aad89f3ed004b94aed1da8b2d
priyank-ramani <202201497@daiict.ac.in>
2026-01-16 15:46:21 +0530

    Hooks-Counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter = 5;
+  let [counter, setCounter] = useState(15);
+  const addValue = () => {
+    // counter++;
+    setCounter(counter++);
+  }
+  const removeValue = () => {
+    // counter--;
+    setCounter(counter--);
+  }
+  return (
+    <>
+      <h1>Hello from upside down</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
